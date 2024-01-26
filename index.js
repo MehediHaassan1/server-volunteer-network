c94e90cdd751ce61c304a027d4803dab904a16c1
Mehedi Hassan Barat <mehedi.haassan1@gmail.com>
2024-01-26 10:40:54 +0600

    database connected

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const cors = require('cors');
-const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+
+require('dotenv').config();
+
 const port = process.env.PORT || 5000;
 
 
@@ -11,6 +15,38 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Volunteer Network server is running!!!')
 })
+
+// --------------------------- DB Start --------------------- //
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.redp2x6.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+
+
+    } finally {
+    }
+}
+run().catch(console.dir);
+
+
+// --------------------------- DB End --------------------- //
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
