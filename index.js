4cf3f0462de10030e2175fa8c2b0d1055f91c48c
Mehedi Hassan Barat <mehedi.haassan1@gmail.com>
2024-01-26 19:10:36 +0600

    jwt applied for more security

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 
@@ -11,6 +12,23 @@ const port = process.env.PORT || 5000;
 // middleware 
 app.use(cors());
 app.use(express.json());
+
+// custom middleware 
+const varifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'Unauthorized Access' })
+    }
+    const token = authorization.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'Forbidden Access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+
+}
 
 app.get('/', (req, res) => {
     res.send('Volunteer Network server is running!!!')
@@ -37,6 +55,13 @@ async function run() {
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
+        // JWT
+        app.post('/access-token', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10h' })
+            res.send({ token })
+        })
+
         const eventsCollection = client.db('volunteerNetwork').collection('events');
         const participantEventCollection = client.db('volunteerNetwork').collection('participantEventCollection')
 
@@ -52,8 +77,12 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/participant-events', async (req, res) => {
+        app.get('/participant-events', varifyJWT, async (req, res) => {
             const email = req.query.email;
+            const decoded = req.decoded.email;
+            if (decoded !== email) {
+                return res.status(401).send({ error: true, message: 'Forbidden Access' })
+            }
             const query = { participantsEmail: email }
             const result = await participantEventCollection.find(query).toArray();
             res.send(result);
